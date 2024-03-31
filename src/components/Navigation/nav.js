4add06089bd8e46f5820d05b1a604ab9cd568137
Ekene Olatunji <31261513+ekeneo@users.noreply.github.com>
2024-03-31 19:11:21 -0400

    Add navigation for mobile

---
+++ src/components/Navigation/nav.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'; 
+import logoImg from '../../assets/Logo/BrainFlix-logo.svg';
+import searchLogo from '../../assets/Icons/search.svg';
+import userIcon from '../../assets/Images/Mohan-muruge.jpg';
+import './nav.scss';
+
+const Navigation = () => {
+  // Define state for the search input value
+  const [searchValue, setSearchValue] = useState('');
+
+  return (
+    <nav className='nav'>
+      <img className='nav__logo' src={logoImg} alt="BrainFlix-logo" />
+      <div className='nav__search-container'>
+        {/* search start */}
+        <input className="nav__search" 
+        type="text" 
+        name="search" 
+        placeholder="Search" 
+        value={searchValue} 
+        onChange={(e) => setSearchValue(e.target.value)}>
+        </input>
+        {/* search end */}
+        <img
+          src={userIcon}
+          alt="User Icon"
+          className="nav__user-icon"
+        />
+      </div>
+      <button className='nav__upload'>Upload</button>
+    </nav>
+  );
+};
+
+export default Navigation;
